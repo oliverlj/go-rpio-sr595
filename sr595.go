cd263415340b3530e4791642f6f2be65ad986c05
oliverlj <o.le.jacques@gmail.com>
2016-10-16 11:44:25 +0200

    first commit

---
+++ sr595.go
@@ -0,0 +1,54 @@
+package sr595
+
+import (
+	"github.com/stianeikeland/go-rpio"
+	"time"
+)
+
+var (
+	numOfRegisterPins int
+	dataPin           = rpio.Pin(22)
+	clockPin          = rpio.Pin(17)
+	latchPin          = rpio.Pin(27)
+	pins              []rpio.State
+)
+
+func Setup(clockPin, dataPin, latchPin rpio.Pin, numberOfPins int) {
+	dataPin.Low()
+	clockPin.Low()
+	latchPin.High()
+	dataPin.Output()
+	clockPin.Output()
+	latchPin.Output()
+	numOfRegisterPins = numberOfPins
+}
+
+func Reset() {
+	clearRegisters()
+	writeRegisters()
+}
+
+func clearRegisters() {
+	for i := 0; i < numOfRegisterPins; i++ {
+		pins[i] = rpio.Low
+	}
+}
+
+func writeRegisters() {
+	latchPin.Low()
+	time.Sleep(time.Microsecond)
+	for i := numOfRegisterPins - 1; i >= 0; i-- {
+		dataPin.Write(pins[i])
+		clockPin.High()
+		time.Sleep(time.Microsecond)
+		clockPin.Low()
+		time.Sleep(time.Microsecond)
+	}
+	latchPin.High()
+	time.Sleep(time.Microsecond)
+}
+
+//set an individual pin HIGH or LOW
+func SetRegisterPin(index int, value rpio.State) {
+	pins[index-1] = value
+}
